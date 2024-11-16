970afa2c07e87380b92da9f74d3f1c93628f788f
Hassan Rahim <hassanrahim26@gmail.com>
2024-11-16 20:53:56 +0530

    updates the counter

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 5)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p>
-          Hello!
         </p>
       </div>
       <p className="read-the-docs">
