682f1c8c75730f0ed759e5b6ddf4b98d28cf4394
Hassan Rahim <hassanrahim26@gmail.com>
2024-11-17 01:37:47 +0530

    adds test case

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/github-pages-demo/'
+  test: {
+    environment: 'jsdom',
+    setupFiles: './setup.js',
+    globals: true
+  }
 })
